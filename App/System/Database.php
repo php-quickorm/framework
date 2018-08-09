735f4a76fd60eaad601ecffe1546dc40664b3ecf
zzfly256 <admin@zzfly.net>
2018-08-09 16:05:03 +0800

    修改 数据库注释

--- App/System/Database.php
+++ App/System/Database.php
@@ -3,20 +3,20 @@ namespace System;
 use System\DatabaseDriver\pdo_mysql;
 class Database{
 
-    /**
-     * PHP-JSORM 框架数据库驱动器
-     *
-     * 传入参数：
-     *      $sql: 需要预处理的 SQL 语句
-     *      $array: 预处理相应的字段数组
-     * 返回值：
-     *      SQL 查询结果的关联数组
-    */
-
     public $db;
 
+    /**
+     * PHP-JSORM 框架数据库类
+     * @author Rytia <rytia@outlook.com>
+     * @param string $sql, string $parameters
+     * @uses 用于驱动数据库，其中 pdo 对象保存在 $this->db
+    */
+
     public function __construct($sql, $parameters = []) {
-        //使用 PDO 进行处理
+        // $sql: 需要预处理的 SQL 语句
+        // $array: 预处理相应的字段数组
+
+        // 使用 pdo_mysql 进行处理
         $databaseConnect = (new pdo_mysql())->connect;
         $toDo = $databaseConnect->prepare($sql);
         $toDo->execute($parameters);
